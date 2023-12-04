130102154a2bbab009037611c74bbb6131f56b93
Tyler Trussell <tyler.trussell@petalcard.com>
2023-12-04 15:53:36 -0600

    day 3 working

--- day3/solve.py
+++ day3/solve.py
@@ -1,4 +1,6 @@
 import argparse
+import pytest
+import functools
 
 
 # (horizontal, vertical)
@@ -17,6 +19,7 @@ def read_input(input_file) -> list[str]:
 def parse_args():
     parser = argparse.ArgumentParser(description="Solve the problem")
     parser.add_argument("input_file", type=str, help="input file")
+    parser.add_argument("mode", type=int, help="part one or part two")
     args = parser.parse_args()
     return args
 
@@ -161,9 +164,113 @@ def test_part_one_solution():
     data = read_input("test_input2.txt") == 413 + 1  # added edge cases
 
 
+def get_number_boundaries(row: int, col: int, data: list) -> (int, int):
+    """Given a number's position, determine its boundaries."""
+    # find the start of the number
+    start_col = col
+    while start_col > 0 and data[row][start_col - 1].isnumeric():
+        start_col -= 1
+
+    # find the end of the number
+    end_col = col
+    while end_col < len(data[row]) - 1 and data[row][end_col + 1].isnumeric():
+        end_col += 1
+
+    return start_col, end_col
+
+
+@pytest.mark.parametrize(
+    "row, col, expected",
+    [
+        (0, 1, (0, 2)),
+        (5, 7, (7, 8)),
+    ]
+)
+def test_get_number_boundaries(row, col, expected):
+    data = read_input("test_input.txt")
+    assert get_number_boundaries(row, col, data) == expected
+
+
+def get_contacted_parts(row: int, col: int, data: list) -> set[tuple]:
+    """Given a point, determine the parts it contacts.
+
+    To avoid counting the same part twice, store the part boundaries as an
+    annoying tuple-of-tuples, which can be stored in a set, e.g.:
+        {((0, 2), (7, 8)),}
+
+    But before returning, convert the set to a list of part numbers.
+    """
+    coordinates = set()
+    for row_d, col_d in VALID_DELTAS:
+        target_ridx = row + row_d
+        target_cidx = col + col_d
+        if (
+            target_ridx < 0
+            or target_ridx >= len(data)
+            or target_cidx < 0
+            or target_cidx >= len(data[target_ridx])
+        ):
+            continue
+        char = data[target_ridx][target_cidx]
+        if char.isnumeric():
+            start, end = get_number_boundaries(target_ridx, target_cidx, data)
+            coordinates.add(((target_ridx, start), (target_ridx, end)))
+
+    parts = list()
+    for start, end in coordinates:
+        start_row, start_col = start
+        end_row, end_col = end
+        part_number = int(data[start_row][start_col:end_col + 1])
+        parts.append(part_number)
+
+    return sorted(parts)  # for tests
+
+
+@pytest.mark.parametrize(
+    "row, col, expected",
+    [
+        (1, 3, [35, 467]),
+        (8, 5, [598, 755]),
+    ]
+)
+def test_get_contacted_parts(row, col, expected):
+    data = read_input("test_input.txt")
+    assert get_contacted_parts(row, col, data) == expected
+
+
+def part_two_solution(data):
+    ttl_ratio = 0
+    for ridx in range(0, len(data)):
+        line = data[ridx]
+        for cidx in range(0, len(line)):
+            char = line[cidx]
+            if char == '*':
+                contacted_parts = get_contacted_parts(ridx, cidx, data)
+                if len(contacted_parts) == 2:
+                    ratio = functools.reduce(lambda x, y: x * y, contacted_parts)
+                    ttl_ratio += ratio
+    return ttl_ratio
+
+
+def test_part_two_solution():
+    # data = read_input("test_input.txt")
+    # assert part_two_solution(data) == 467835
+    data = read_input("test_input2.txt")
+    print(data)
+    assert part_two_solution(data) == 6756
+
+
+MODE_MAP = {
+    1: part_one_solution,
+    2: part_two_solution,
+}
+
+
 def main():
     args = parse_args()
-    print(part_one_solution(read_input(args.input_file)))
+    data = read_input(args.input_file)
+    func = MODE_MAP[args.mode]
+    print(func(data))
 
 
 if __name__ == "__main__":
